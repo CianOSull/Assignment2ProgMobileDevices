d0d03941d52b98d4868dc904b3c33de5779e1171
Cian O'Sullivan <cian.osullivan4@mycit.ie>
2019-12-08 19:47:15 +0000

    Implemented context menu in main activity and created contxt menu in menu

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -110,6 +111,32 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         });
 
+        // Context menu
+        registerForContextMenu(changeActivityBtn);
+    }
+
+    // Context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option_1:
+                Toast.makeText(this, "Option 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.option_2:
+                Toast.makeText(this, "Option 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     private void lockScreenPage(){
